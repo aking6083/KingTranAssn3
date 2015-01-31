ed54de971e409302367f4ed4bafdd6365491a4f9
ctran88 <christopher.v.tran@gmail.com>
2015-01-30 18:25:39 -0600

    Latest changs with working randomNums()

--- kingTranAssn3/KingTran-assn3-main.cpp
+++ kingTranAssn3/KingTran-assn3-main.cpp
@@ -29,23 +29,16 @@ int main()
 	//Get table size
 	tableSize = getTableSize();
 
+
 	// Testing these functions. CT
 	openTable = initOpenTable(tableSize);
 	makeOpenTable(randomNums, openTable, tableSize, probe);
 	
-	cout << "random table[0]: " << randomNums[0] << endl
-		<< "random table[1]: " << randomNums[1] << endl
-		<< "random table[2]: " << randomNums[2] << endl
-		<< "random table[3]: " << randomNums[3] << endl
-		<< "random table[4]: " << randomNums[4] << endl;
-
-	cout << "open table[0]: " << openTable[0] << endl
-		<< "open table[1]: " << openTable[1] << endl
-		<< "open table[2]: " << openTable[2] << endl
-		<< "open table[3]: " << openTable[3] << endl
-		<< "open table[4]: " << openTable[4] << endl
-		<< "open table[5]: " << openTable[5] << endl
-		<< "open table[6]: " << openTable[6] << endl;
+	for (int i = 0; i < LIST_SIZE; i++)
+		cout << "RandomIndex[" << i << "]: " << randomNums[i] << endl;
+	
+	for (int x = 0; x < tableSize; x++)
+		cout << "OpenTable[" << x << "]: " << openTable[x] << endl;
 	// End of test. CT
 
 	system("PAUSE");
